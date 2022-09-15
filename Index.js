f70613c72be41f708c9f017fe7e980143a37cf61
ManoellyBernardo <manoellymaria@gmail.com>
2022-09-15 20:30:25 -0300

    Atualiazação

---
+++ Index.js
@@ -0,0 +1,13 @@
+console.log("Hello World")
+console.log(5)
+console.log(43.78)
+console.log(true)
+console.log(false)
+
+let mensagem = "Olá, mundo!"
+
+console.log(mensagem)
+
+mensagem = "Alteração de mensagem"
+
+console.log(mensagem)
