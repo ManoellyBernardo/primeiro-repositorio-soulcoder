f715db510686cdcd2e36070381d2900d08b72ae5
ManoellyBernardo <manoellymaria@gmail.com>
2022-09-21 17:28:42 -0300

    Estrutura If com JavaScript

---
+++ desafio2.js
@@ -0,0 +1,40 @@
+/**
+ * Um posto está vendendo combustíveis com a seguinte tabela de descontos:
+ * Álcool = até 25L = 2% off
+ *          acima de 25L = 4% off
+ * Gasolina = até 25L = 3% off
+ *           acima de 25L = 5% off 
+ * Escreva um algoritimo que leia o número de litros vendidos e o tipo de combustível (A para Alcool e G para gasolina), calcule e imprima o valor a ser pagp pelo cliente.
+ * Sabendo-se que o 1l de gasolina = R$2,70 e 1l de álcool = R$1,90
+ */
+
+const tipoCombustivel = prompt(`
+Qual será o tipo de combustível ?
+
+Digite A se for álcool;
+Digite G se for gasolina.
+`)
+
+if (tipoCombustivel == "A") {
+    
+    const litrosCombustivel = parseInt(prompt("Quantos litros de álcool você irá colocar ?"))
+    if (litrosCombustivel <= 25) {
+        const precoAlcool = litrosCombustivel * 1.9 - litrosCombustivel * 0.2
+        alert("Você irá pagar " + precoAlcool)
+    } else {
+        const precoAlcool = litrosCombustivel * 1.9 - litrosCombustivel * 0.4
+        alert("Você irá pagar " + precoAlcool)
+    }
+}
+
+if (tipoCombustivel == "G") {
+    
+    const litrosCombustivel = parseInt(prompt("Quantos litros de gasolina você irá colocar ?"))
+    if (litrosCombustivel <= 25) {
+        const precoGasolina = litrosCombustivel * 2.7 - litrosCombustivel * 0.3
+        alert("Você irá pagar " + precoGasolina)
+    } else {
+        const precoGasolina = litrosCombustivel * 2.7 - litrosCombustivel * 0.5
+        alert("Você irá pagar " + precoGasolina)
+    }
+}
